4f0f55bf0d0aab3f0a0d2892e2163a6721d0a624
Dom Eccleston <domeccleston@gmail.com>
2020-02-19 15:14:26 +0000

    add caching

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,8 +5,14 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
-  pass
+def eating_cookies(n, cache={}):
+	if n < 0:	
+		return 0
+	elif n == 0:
+		return 1
+	elif n > 0 and not n in cache:
+		cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+	return cache[n]
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
